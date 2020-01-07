f7e3a5d20a929dd84a792ab654ba794832627bc9
Wilfried Woivre <wilfried.woivre@gmail.com>
2020-01-07 16:03:58 +0100

    add lab end

--- routes/keyvault.js
+++ routes/keyvault.js
@@ -1,9 +1,33 @@
 var express = require('express')
 var router = express.Router();
+const { ManagedIdentityCredential, ClientSecretCredential } = require('@azure/identity');
+const { SecretClient } = require('@azure/keyvault-secrets');
+
+const tenantId = process.env.TENANT_ID;
+const clientId = process.env.CLIENT_ID;
+const secret = process.env.CLIENT_SECRET;
+
+const vaultName = process.env.KEYVAULT_NAME;
+const vaultUri = `https://${vaultName}.vault.azure.net/`;
 
 
 router.get('/', async function (req, res, next) {
+    let credentials;
+
+    if (process.env.USE_MSI) {
+        credentials = new ManagedIdentityCredential();
+    }
+    else {
+        credentials = new ClientSecretCredential(tenantId, clientId, secret);
+    }
+
+    let client = new SecretClient(vaultUri, credentials);
     let secrets = [];
+
+    for await (const secret of client.listPropertiesOfSecrets()) {
+        let secretData = await client.getSecret(secret.name);
+        secrets.push({ name: secret.name, value: secretData.value })
+    }
 
     res.render('keyvault', { title: 'List Azure Key Vault Secrets', secrets: secrets });
 });
